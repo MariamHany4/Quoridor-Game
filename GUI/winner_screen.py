0f10fffcdd16281ba7f0da12d382d68636c55f3b
(ENG) <167337048+Toqa-Gamal@users.noreply.github.com>
2025-12-17 10:57:14 +0200

    Update winner screen GUI to reflect wall/game state correctly

--- GUI/winner_screen.py
+++ GUI/winner_screen.py
@@ -2,7 +2,6 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
-
 # ---------------- WINNER VIEW ----------------
 class WinnerView(QWidget):
     def __init__(self, parent=None):
